d1c7d8d1fa766c4c286f4d8fdd9acad1ed64031e
Priyansu471 <mishrapriyanshu2003@gamil.com>
2023-09-04 03:39:15 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,184 @@
+const content = document.querySelector(".content");
+const dropZone = document.querySelector(".drop-zone");
+const fileInput = document.querySelector("#fileInput");
+const browseBtn = document.querySelector("#browseBtn");
+
+const bgProgress = document.querySelector(".bg-progress");
+const progressPercent = document.querySelector("#progressPercent");
+const progressContainer = document.querySelector(".progress-container");
+const progressBar = document.querySelector(".progress-bar");
+const status = document.querySelector(".status");
+
+const sharingContainer = document.querySelector(".sharing-container");
+const copyURLBtn = document.querySelector("#copyURLBtn");
+const fileURL = document.querySelector("#fileURL");
+const emailForm = document.querySelector("#emailForm");
+
+const toast = document.querySelector(".toast");
+
+const baseURL = "https://file-expressapi.onrender.com";
+const uploadURL = `${baseURL}/api/v1/files`;
+const emailURL = `${baseURL}/api/v1/send`;
+
+const maxAllowedSize = 200 * 1024 * 1024; //200MB
+
+browseBtn.addEventListener("click", () => {
+  fileInput.click();
+});
+
+dropZone.addEventListener("drop", (e) => {
+  e.preventDefault();
+  console.log("dropped", e.dataTransfer.files[0].name);
+  const files = e.dataTransfer.files;
+  if (files.length === 1) {
+    if (files[0].size < maxAllowedSize) {
+      fileInput.files = files;
+      uploadFile();
+    } else {
+      showToast("Max file size is 200MB");
+    }
+  } else if (files.length > 1) {
+    showToast("You can't upload multiple files");
+  }
+  dropZone.classList.remove("dragged");
+});
+
+dropZone.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  dropZone.classList.add("dragged");
+
+  // console.log("dropping file");
+});
+
+dropZone.addEventListener("dragleave", (e) => {
+  dropZone.classList.remove("dragged");
+
+  console.log("drag ended");
+});
+
+// file input change and uploader
+fileInput.addEventListener("change", () => {
+  if (fileInput.files[0].size > maxAllowedSize) {
+    showToast("Max file size is 100MB");
+    fileInput.value = ""; // reset the input
+    return;
+  }
+  uploadFile();
+});
+
+// sharing container listenrs
+copyURLBtn.addEventListener("click", () => {
+  fileURL.select();
+  document.execCommand("copy");
+  showToast("Copied to clipboard");
+});
+
+fileURL.addEventListener("click", () => {
+  fileURL.select();
+});
+
+const uploadFile = () => {
+  console.log("Uploading your file...");
+
+  files = fileInput.files;
+  const formData = new FormData();
+  formData.append("myfile", files[0]);
+
+  //show the uploader
+  progressContainer.style.display = "block";
+
+  // upload file
+  const xhr = new XMLHttpRequest();
+
+  // listen for upload progress
+  xhr.upload.onprogress = function (event) {
+    // find the percentage of uploaded
+    let percent = Math.round((100 * event.loaded) / event.total);
+    progressPercent.innerText = percent;
+    const scaleX = `scaleX(${percent / 100})`;
+    bgProgress.style.transform = scaleX;
+    progressBar.style.transform = scaleX;
+  };
+
+  // handle error
+  xhr.upload.onerror = function () {
+    showToast(`Error in upload: ${xhr.status}.`);
+    fileInput.value = ""; // reset the input
+  };
+
+  // listen for response which will give the link
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      onFileUploadSuccess(xhr.responseText);
+    }
+  };
+
+  xhr.open("POST", uploadURL);
+  xhr.send(formData);
+};
+
+const onFileUploadSuccess = (res) => {
+  fileInput.value = ""; // reset the input
+  status.innerText = "Uploaded";
+
+  // remove the disabled attribute from form btn & make text send
+  emailForm[2].removeAttribute("disabled");
+  emailForm[2].innerText = "Send";
+  progressContainer.style.display = "none"; // hide the box
+
+  const { file: url } = JSON.parse(res);
+  console.log(url);
+  sharingContainer.style.display = "block";
+  dropZone.style.display = "none";
+  content.innerHTML = `<h1>File uploaded successfully</h1>
+  <h3>Share this link with your buddy</h3>
+  <h3>or surprise them with mail</h3>`;
+  fileURL.value = url;
+};
+
+emailForm.addEventListener("submit", (e) => {
+  e.preventDefault(); // stop submission
+
+  // disable the button
+  emailForm[2].setAttribute("disabled", "true");
+  emailForm[2].innerText = "Sending";
+
+  const url = fileURL.value;
+
+  const formData = {
+    uuid: url.split("/").splice(-1, 1)[0],
+    emailFrom: emailForm.elements["from-email"].value,
+    emailTo: emailForm.elements["to-email"].value,
+    name: emailForm.elements["name"].value,
+  };
+  console.log(formData);
+  fetch(emailURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.sucess) {
+        showToast(data.message);
+        sharingContainer.style.display = "none";
+        dropZone.style.display = "flex";
+        content.innerHTML = `<h1>Your files are safe with us!</h1>
+        <h3>Just upload file and link will be availabe to share</h3>
+        <h3>Do you know you can also share download link via email now</h3>`; // hide the box
+      }
+    });
+});
+
+let toastTimer;
+// the toast function
+const showToast = (msg) => {
+  clearTimeout(toastTimer);
+  toast.innerText = msg;
+  toast.classList.add("show");
+  toastTimer = setTimeout(() => {
+    toast.classList.remove("show");
+  }, 2000);
+};
